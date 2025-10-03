c57116ec68bae04442e4905192fd91c8d1ce6e5c
Deachono6 <benzalone49@gmil.com>
2025-10-03 12:09:53 +0700

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/todos', 
+  baseURL: 'https://todo-server-gules.vercel.app/api/todos', 
 });
 
 export default api;
